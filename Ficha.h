e5eb6225b9c64845f69c6c20324a3f772e743eec
Ramon Alves <alvesramon.lima@gmail.com>
2019-04-19 11:07:28 -0300

    Criação de Ficha.h, tudo aqui presente será diretamente relacionado ao arquivo Ficha.cpp

---
+++ Ficha.h
@@ -0,0 +1,21 @@
+/*
+    Aqui será disposto tudo que será utilizado no arquivo Ficha.cpp, importante lembrar que essa primeira 
+    Ficha.cpp será apenas para o sistema de 7o Mar.
+
+    Ps.: V0.1
+*/
+#ifndef FICHA_H_
+#define FICHA_H_ 
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Ficha
+{
+
+
+};
+
+#endif
