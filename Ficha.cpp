1e6e3d4f022c78fbae830e7389898bc02a8bee26
Ramon Alves <alvesramon.lima@gmail.com>
2019-04-19 11:20:34 -0300

    Criação da Ficha.cpp responsável por rodar as funções que irão de fato fazer acontecer o programa.

---
+++ Ficha.cpp
@@ -0,0 +1,15 @@
+/*  Funções da classe "Ficha":
+    -Adicionar Campanha -> (Create_campaign)
+        i.Verificação do nome da campanha -> (Campaign_verification)
+    -Criar Ficha -> (Create_character)
+        i.Verificação do nome da ficha -> (Character_name_verification)
+    
+    Ps.: Ficha.h é exclusivamente para 7o Mar, por enquanto até resolver a questão de herança.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include "Ficha.h"
+
+using namespace std;
