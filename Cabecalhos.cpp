0f2d30d60c3049f7bd3cbd5c757cc2f46854e233
Ramon Alves <alvesramon.lima@gmail.com>
2019-04-19 10:57:46 -0300

    Cabeçalho de apresentação do programa

--- Cabecalhos.cpp
+++ Cabecalhos.cpp
@@ -6,5 +6,24 @@
     -Cabeçalho de exposição de todas as profissões já com suas respectivas pericias e vantagens -> (Professions)
 
     Ps.: Interessante acrescentar aqui nesse número de versão sempre que atualizado ou commitado por nós. 
-    Ps.: V0.1
+    Ps.: V0.2
 */
+
+#include <iostream>
+using namespace std;
+
+void Presentation()
+{
+    cout << "      ___________________________________________________ "  << endl;
+    cout << "     |                                                   |" << endl;
+    cout << "     |Seja bem vindo ao gerenciador de fichas de rpg V1.0|" << endl;
+    cout << "     |                                                   |" << endl;
+    cout << "     |Neste programa será possível a criação de fichas e |" << endl; 
+    cout << "     |gerenciamentos de campanhas. O nosso objetivo é aju|" << endl;
+    cout << "     |dar o mestre e toda sua mesa a se organizarem da me|" << endl;
+    cout << "     |lhor e mais simplista forma para que toda diversão |" << endl;
+    cout << "     |seja totalmente garantida. Na versão atual temos su|" << endl;
+    cout << "     |porte para o sistema de 'Tormenta Rpg' e '7o Mar 2a|" << endl;
+    cout << "     |edição'. Então aproveite! De jogador para jogador! |" << endl;
+    cout << "     |___________________________________________________|" << endl;
+}
