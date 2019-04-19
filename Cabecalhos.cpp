42202b6c432f58bf351424146997ef0c451883f2
Ramon Alves <alvesramon.lima@gmail.com>
2019-04-19 10:51:04 -0300

    Arquivo que simplesmente irá ter todos os cabeçalhos, tabelas, textos expositivos. A criação deste é para ficar mais fácil a utilização por chamada de função.

---
+++ Cabecalhos.cpp
@@ -0,0 +1,10 @@
+/*
+    Cabeçalhos:
+    -Apresentação -> (Presentation)
+    -Escolher Sistema -> (Choose_system)
+    -Opção 7o Mar (2a Edição) -> (Sea_menu)
+    -Cabeçalho de exposição de todas as profissões já com suas respectivas pericias e vantagens -> (Professions)
+
+    Ps.: Interessante acrescentar aqui nesse número de versão sempre que atualizado ou commitado por nós. 
+    Ps.: V0.1
+*/
