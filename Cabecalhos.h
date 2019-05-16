2d54c1b1ae444b09dba9425c04e5004be6c072d4
Ramon Alves <alvesramon.lima@gmail.com>
2019-05-16 16:55:48 -0300

    Novo arquivo, .h de Cabecalhos

---
+++ Cabecalhos.h
@@ -0,0 +1,19 @@
+#ifndef CABECALHOS_H_
+#define CABECALHOS_H_ 
+
+#include <iostream>
+#include <cstring>
+
+class Cabecalhos
+{   
+    private:
+    public:
+    void Presentation();
+    void Choose_system();
+    void Sea_menu();
+    void Professions();
+    void Advantages();
+
+};
+
+#endif
